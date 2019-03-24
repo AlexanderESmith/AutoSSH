6227dc4c561b6a215c5e6fc4d178c1166389d44a
AlexanderESmith <github@AlexanderESmith.com>
2019-03-24 13:37:53 -0700

    First upload to git
    Added from previous version;
    - Integrated rssh.bash (which would prompt to reconnect on connection loss/reset)

---
+++ assh.bash
@@ -0,0 +1,159 @@
+#!/bin/bash
+ 
+############## START USER CONFIGURABLE ##############
+# Where necessary, also update the shebang above.
+#       It should be bash, not tsch, csh, or sh.
+# The below line should work in most cases, if you
+#       leave assh.bash and rssh.bash in your home
+SCRIPT_PATH="~/"
+# Set the string below to the header you want to see above the list
+HEADER="a=Azure"$'\n'"l=Legacy(AWS)"$'\n'"v=VPC(AWS)"$'\n'"o=Office(external)"$'\n'"lo=Office(local)"$'\n'
+############### END USER CONFIGURABLE ###############
+ 
+# Used to define something the user specifically wants to see
+USERFILTER=""
+# Used to hide things the user never needs, but still need to be in the file
+# Grep Regex format
+SYSTEMFILTER="bastion-o|bastion-v"
+FANCYDISP=0
+clear
+while true
+do
+#   clear
+if [[ -z $1 ]]; then
+		echo
+		COUNT=1
+		DISPCOUNT=1
+		HOSTDISPLIST=''
+		HOSTACTLIST=''
+		HOSTS=''
+		HOSTSTEMP=''
+		for HOSTITEM in $(cat ~/.ssh/config | grep host | grep -v hostname | grep -v "*" \
+			| grep -Ev "^#" | awk -F' ' '{print $2}' | sort)
+		do
+			# Decide of a host matches the filters, and add it to the list
+			ADDEDHOST=$HOSTITEM
+			for FILTERITEM in $(echo "$USERFILTER")
+			do
+				ADDEDHOST="$(echo $ADDEDHOST | grep -E $FILTERITEM)"
+			done
+			HOSTS="$HOSTS $ADDEDHOST"
+			# Display during search
+			if [ "$FANCYDISP" == "1" ]; then
+				HOSTSTEMP="$(echo "$HOSTS" | sed 's/ /\n/g' | column -x)"
+				if [ -n "$ADDEDHOST" ] ; then
+					DISPADDEDHOST=$ADDEDHOST
+				fi
+				if [ "$DISPCOUNT" -lt "10" ] ; then
+					printf "    $DISPCOUNT $DISPADDEDHOST                                                                         \r"
+				elif [ "$DISPCOUNT" -lt "100" ] ; then
+					printf "   $DISPCOUNT $DISPADDEDHOST                                                                         \r"
+				else
+					printf "  $DISPCOUNT $DISPADDEDHOST                                                                         \r"
+				fi
+				((DISPCOUNT++))
+			fi
+		done
+		for HOST in $(echo $HOSTS)
+		do
+			if [ "$COUNT" -lt "10" ] ; then
+				HOSTDISPLIST="$HOSTDISPLIST   $COUNT: $HOST"$'\n'
+			elif [ "$COUNT" -lt "100" ] ; then
+				HOSTDISPLIST="$HOSTDISPLIST  $COUNT: $HOST"$'\n'
+			else
+				HOSTDISPLIST="$HOSTDISPLIST $COUNT: $HOST"$'\n'
+			fi
+			HOSTACTLIST[$COUNT]="$HOST"
+			#HOSTACTLIST[$COUNT]="$HOST"$'\n'
+			((COUNT++))
+			done
+			clear
+			echo
+			echo $HEADER | column -t
+			echo
+			echo "$HOSTDISPLIST" | column
+			echo
+			if [ "$USERFILTER" ]; then
+				echo "Current filter:"
+				echo "$USERFILTER"
+				echo
+			fi
+			echo '[blank] - Refresh'
+			echo '	 f - [F]ilter (edit current)'
+			echo '	ff - [FF]ilter (blank prompt)'
+			echo '	 e - [E]dit'
+			echo '	 c - Show [c]onfig for listed servers'
+			echo "	 d - Toggle fancy [D]isplay (current: $FANCYDISP)"
+			echo '	 x - E[x]it'
+			echo
+			read -p "Select server: " SELECTION
+			case $SELECTION in
+			'')
+				# Catching blank entry. Do nothing, restart loop.
+			;;
+			c)
+				read -p "Showing config lines for listed servers in 'less'; (press enter to continue...)"
+				CONFIGLINES="$(echo "")"
+				for THISHOST in $HOSTDISPLIST; do
+					CONFIGLINES="$CONFIGLINES"$'\n'"$(cat ~/.ssh/config | grep -Ev '^#' | sed -e "/host $THISHOST/,/host /!d" | head -n"-1")"
+					#echo '~~~~~~'
+				done
+				CONFIGLINES="$CONFIGLINES"$'\n'"$(echo "")"
+				echo "$CONFIGLINES"$ | grep -Ev '^$|^\s*$' | less
+			;;
+			e)
+				#Exit condition
+				vim ~/.ssh/config
+			;;
+			d)
+				if [ "$FANCYDISP" == "0" ]; then
+					FANCYDISP=1
+				else
+					FANCYDISP=0
+				fi
+			;;
+			x)
+				#Exit condition
+				break
+			;;
+			ff)
+				# Filter condition
+				echo
+				echo "Blank filters show all"
+				echo "Space-seperated list matches servers containing all terms"
+				read -p "Define filter (blank to clear filter): " -e USERFILTER
+			;;
+			f)
+				# Filter condition
+				echo
+				echo "Blank filters show all"
+				echo "Space-seperated list matches servers containing all terms"
+				read -p "Define filter (blank to clear filter): " -i "$USERFILTER" -e USERFILTER
+			;;
+			[0-9]*)
+				# if condition to determine if I need a new screen tab, or just to start RDP
+				if [[ "${HOSTACTLIST[$SELECTION]}" =~ ^rdp ]]; then
+					screen -t ${HOSTACTLIST[$SELECTION]} bash -c "bash /home/asmith/acefile/versium/rdp/${HOSTACTLIST[$SELECTION]}.bash"
+				else
+					# Newtab/Connection condition
+									ASSHRESULT="$(screen -Q select ${HOSTACTLIST[$SELECTION]})"
+									if [[ -n "$ASSHRESULT" ]]; then
+											screen -t ${HOSTACTLIST[$SELECTION]} bash -c "bash ${SCRIPT_PATH}/assh.bash ${HOSTACTLIST[$SELECTION]}"
+									fi
+				fi
+			;;
+			*)
+				echo "Invalid input..."
+				read -p ''
+		;;
+		esac
+else
+	while true
+	do                     
+		ssh $1
+		echo
+		echo '-- connction terminated --'
+		read -p 'Reconnect = [enter] | Close = [^c]'
+	done
+fi
+done
